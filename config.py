e24f71cfc967ef7e6ae95f3439074930e252ec07
arsurus <arsurus@gmail.com>
2023-07-03 09:14:09 +0300

    3jul

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Внесение актуальных токенов
 community_token = 'vk1.a.fHqR92UKFI-my_secret_token'
 access_token = 'vk1.a.FKTphMyLmJGQ-my_secret_token'
-# Указание БД
+# Указание БД для хранения поиска
 db_url_object = 'postgresql://postgres:postgres@localhost:5432/vkbot'
